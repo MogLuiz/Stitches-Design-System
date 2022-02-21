fdecfae162ad7041d10e99cd4e70c34c0bfae2e4
Luiz Henrique <luizhjramos@outlook.com>
2022-02-21 17:25:00 -0300

    feat: stitches global style application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,11 @@ import React from "react";
 // Types
 import { AppProps } from "next/app";
 
+// Stitches Styles
+import { globalStyles } from "@/styles/global";
+
+globalStyles();
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
