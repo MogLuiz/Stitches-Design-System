16bfaddbf221d69837c929353ae30c3e723a4370
Luiz Henrique <luizhjramos@outlook.com>
2022-02-21 18:37:01 -0300

    feat: Button Stitches component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,24 @@ const Flex = styled("div", {
   display: "flex",
 });
 
+const Button = styled("button", {
+  border: 0,
+  appearance: "none",
+  cursor: "pointer",
+
+  transition: "background 0.2s",
+  fontWeight: "bold",
+});
+
 const Home: React.FC = () => {
   return (
     <Flex css={{ gap: "$16", padding: "$16" }}>
-      <button type="button">Experts Club</button>
-      <button type="button">Experts Club</button>
+      <Button type="button" color="primary">
+        Experts Club
+      </Button>
+      <Button type="button" color="shape" size="full">
+        Experts Club
+      </Button>
     </Flex>
   );
 };
