bede2b30acbd41b2927db79d521e3f15b657c579
Luiz Henrique <luizhjramos@outlook.com>
2022-02-21 18:43:07 -0300

    feat: color and size stitches variants

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,33 @@ const Button = styled("button", {
 
   transition: "background 0.2s",
   fontWeight: "bold",
+
+  variants: {
+    color: {
+      primary: {
+        backgroundColor: "$primary",
+        color: "#fff",
+      },
+      shape: {
+        backgroundColor: "$shape",
+        color: "#fff",
+      },
+    },
+    size: {
+      small: {
+        padding: "$16 $32",
+      },
+      full: {
+        padding: "$16 $32",
+        flex: 1,
+      },
+    },
+  },
+
+  defaultVariants: {
+    color: "primary",
+    size: "small",
+  },
 });
 
 const Home: React.FC = () => {
