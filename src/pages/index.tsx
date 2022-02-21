9dbf6bd4f4661d636c0fcd27cd2481ca91c6004c
Luiz Henrique <luizhjramos@outlook.com>
2022-02-21 16:59:15 -0300

    feat: flex stitches styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
 // Packages
 import React from "react";
 
+// Stitches Styles
+import { styled } from "@/styles";
+
+const Flex = styled("div", {
+  display: "flex",
+});
+
 const Home: React.FC = () => {
-  return <h1>Hello Experts Club</h1>;
+  return (
+    <Flex css={{ gap: "$16", padding: "$16" }}>
+      <button type="button">Experts Club</button>
+      <button type="button">Experts Club</button>
+    </Flex>
+  );
 };
 
 export default Home;
